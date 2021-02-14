030b617858a70224c336b45c8b55a2e97946c471
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-02-14 10:56:38 +0900

    関数コンポーネントでライフサイクルを扱う

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-// import LikeButton from './LikeButton'
+import LikeButton from './LikeButton'
 
 const Article = (props) => {
     const [isPublished, togglePublished] = useState(false)
@@ -14,7 +14,7 @@ const Article = (props) => {
             <h2>{props.title}</h2>
             <label htmlFor="check">公開状態</label>
             <input type="checkbox" checked={isPublished} id="check" onClick={() => togglePublished(!isPublished)}/>
-            {/* <LikeButton count={props.count}/> */}
+            <LikeButton />
             {/* <p>順番は{props.order}</p>
             <p>著者：{props.authorName}</p>
             <p>{publishState}</p> */}
